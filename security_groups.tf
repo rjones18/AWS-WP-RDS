c178076e985c1d92aee7d26f930f86777ee2b8ec
rjones18 <regjones16@outlook.com>
2023-07-30 19:51:34 -0500

    Initial commit

--- security_groups.tf
+++ security_groups.tf
@@ -3,13 +3,13 @@ resource "aws_security_group" "database-security-group" {
   description = "Allow access to DB instance"
   vpc_id      = data.aws_vpc.main_vpc.id
 
-  ingress {
-    description     = "Allow EC2 instance to access RDS Database"
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = ["sg-070625d8d8e9ae37b"]
-  }
+  // ingress {
+  //   description     = "Allow EC2 instance to access RDS Database"
+  //   from_port       = 3306
+  //   to_port         = 3306
+  //   protocol        = "tcp"
+  //   security_groups = ["sg-070625d8d8e9ae37b"]
+  // }
 
   # OUTBOUD CONNECTIONS
   egress {
